36e95e6f685e9e4ca420b744cf87153e824ec91b
TomilovES <blat13@gmail.com>
2023-04-18 16:51:30 +0300

    тз-9 v.5

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -31,7 +31,7 @@ public class UserController {
     }
 
     @PutMapping
-    public User update(@RequestBody  User user){
+    public User update(@RequestBody  User user) {
         validateService.validateUser(user);
         return repository.update(user);
     }
