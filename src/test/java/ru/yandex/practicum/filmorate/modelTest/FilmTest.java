bd77acabd6edc0508274fd4031a8a566d42c174f
TomilovES <blat13@gmail.com>
2023-05-30 22:05:43 +0300

    тз-11 checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/modelTest/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/modelTest/FilmTest.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.modelTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
+
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -12,7 +13,7 @@ import java.util.Set;
 
 public class FilmTest {
     private final Film correctFilmData = Film.builder().
-            name("В бой идут одни «старики»")
+            name("В бой идут одни старики")
             .description("Фильм, посвящённый тематике Великой Отечественной войны")
             .releaseDate(LocalDate.parse("1974-09-12"))
             .duration(87)
@@ -20,7 +21,7 @@ public class FilmTest {
     private final Film incorrectFilmData = Film.builder().
             name("В бой идут одни «старики»")
             .description("Эта эскадрилья стала «поющей» - так капитан Титаренко подбирал себе новичков. Его" +
-                    " «старикам» было не больше двадцати, но «желторотиков», пополнение из летных училищ ускоренного" +
+                    " старикам было не больше двадцати, но желторотик ов, пополнение из летных училищ ускоренного" +
                     " выпуска, в бой все равно, по возможности, не пускали..")
             .releaseDate(LocalDate.parse("1125-09-10"))
             .duration(-87)
