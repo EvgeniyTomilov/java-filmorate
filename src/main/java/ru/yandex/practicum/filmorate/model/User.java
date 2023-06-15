3b6626315bea7c80dfefc24175f3b0908d6bc823
TomilovES <blat13@gmail.com>
2023-06-15 16:20:54 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
-import java.util.Set;
 
 @Data
 @Builder
